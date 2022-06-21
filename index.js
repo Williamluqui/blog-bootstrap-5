73a82918db18574f0eab57918b959c0d0889572b
William Luqui <william.luqui@gmail.com>
2022-06-21 01:49:16 -0300

    melhorias de paginacao

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
 const categoriesController = require("./routes/categories/CategoriesController");
 const articlesController = require("./routes/articles/ArticlesController");
-const connection = require("./database/database")
+const connection = require("./database/database");
 const port = process.env.PORT;
 
-const Article = require("./routes/articles/Article")
-const Category = require("./routes/categories/Category")
+const Article = require("./routes/articles/Article");
+const Category = require("./routes/categories/Category");
 
 // VIEW ENGINE
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // STATIC CCS
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // BODY PARSER
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 //ROUTES
 app.use("/", categoriesController);
 app.use("/", articlesController);
 
-app.get("/",(req, res)=>{
-// PAGINA HOME //
-  Article.findAll({
-    order:[['id','DESC']]
-  }).then(articles=>{
-      Category.findAll().then(category=>{
-        res.render("homepage",{articles: articles, category:category})
-        
-    })
+app.get("/", (req, res) => {
+  // PAGINA HOME //
 
- })
+  Article.findAll({
+    order: [["id", "DESC"]],
+    limit: 4,
+  }).then((articles) => {
+    Category.findAll().then((category) => {
+      res.render("homepage", { articles: articles, category: category });
+    });
+  });
 });
 
+app.use((req, res) => {
+  res.render("404");
+});
 
-app.use((req,res)=>{
-    res.render('404')
-})
-
-//DATABASE 
+//DATABASE
 connection
-.authenticate()
-.then(()=>{
-    console.log('Banco de dados ok')
-}).catch((error)=>{
+  .authenticate()
+  .then(() => {
+    console.log("Banco de dados ok");
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
-
-app.listen(port,()=>{
-    console.log("**Servidor On**")
-})
+app.listen(port, () => {
+  console.log("**Servidor On**");
+});
--- routes/articles/ArticlesController.js
+++ routes/articles/ArticlesController.js
@@ -1,108 +1,102 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../categories/Category")
+const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
-const flash = require("connect-flash")
-const session = require('express-session');
+const flash = require("connect-flash");
+const session = require("express-session");
 
-router.use(flash())
-router.use(session({
-  secret:'secret',
-  cookie:{maxAge:60000},
-  resave: false,
-  saveUninitialized:false
-}));
-
-
-router.get("/admin/articles",(req, res) =>{
-  Article.findAll({
-    include: [{model:Category}]
-  }).then(articles => {
-    res.render("admin/articles/index",{ articles: articles.filter(a => a.category != undefined) });
+router.use(flash());
+router.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
   })
+);
+
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", {
+      articles: articles.filter((a) => a.category != undefined),
+    });
+  });
 });
 // NOVO ARTIGO
-router.get("/admin/articles/new",(req, res) => {
-  Category.findAll().then(categories =>{
-    res.render("admin/articles/new",{categories:categories})
-    
-  }).catch(()=>{
-    res.status(401).send({error:true ,msg: "Não encontrado"})
-  })
-  
-})
-router.get("/post/:slug",(req,res)=>{
-  let slug = req.params.slug
- 
-
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll()
+    .then((categories) => {
+      res.render("admin/articles/new", { categories: categories });
+    })
+    .catch(() => {
+      res.redirect("404")
+    });
+});
+router.get("/post/:slug", (req, res) => {
+  let slug = req.params.slug;
 
   Article.findOne({
-    where:{
-      slug:slug,
-      
+    where: {
+      slug: slug,
     },
-    include:[{model:Category}]
-  })
-  .then(article=>{
-    if(article != undefined){
-      res.render("posts",{article:article});
+    include: [{ model: Category }],
+  }).then((article) => {
+    if (article != undefined) {
+      res.render("posts", { article: article });
     }
-    
-    // console.log(article.title)
-  })
- 
-})
+  });
+});
 
-
-
-
-router.get("/admin/articles/edit/:slug",(req, res)=>{
+router.get("/admin/articles/edit/:slug", (req, res) => {
   // EDITAR ARTIGO
   let slug = req.params.slug;
-  let categories = req.params.title
   
-
-  Article.findOne({where:{slug:slug}})
-  .then(article=>{
-    Category.findAll().then(category=>{
-      res.render("admin/articles/edit",{article: article, category:category})
+  Article.findOne({ where: { slug: slug } }).then((article) => {
+    Category.findAll().then((category) => {
+      if (article != undefined) {
+        res.render("admin/articles/edit", {
+        article: article,
+        category: category,
+      });
+        
+      }else{
+        res.render('admin/articles/')
+      }
       
-  })
-})
-})
-
-
-
-
-router.post("/article/update",(req, res)=>{
-  //ATUALIZAR O BD APOS EDITAR O ARQUIVO
-  let id = req.body.id
-  let title= req.body.title;
-  let body = req.body.body
-  let category = req.body.category
-    
-      Article.update({title:title, slug:slugify(title),body:body,categoryId:category },{
-         where: {
-         id:id
-         
-    }
-  }).then(()=>{
-    res.redirect("/admin/articles")
-    console.log(slugify(title) , title, body,category)
-    
-  }).catch((err)=>{
-    res.redirect('/').status(401)
-  })
+    });
   });
+});
 
+router.post("/article/update", (req, res) => {
+  //ATUALIZAR O BD APOS EDITAR O ARQUIVO
+  let id = req.body.id;
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
 
+  Article.update(
+    { title: title, slug: slugify(title), body: body, categoryId: category },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then(() => {
+      res.redirect("/admin/articles") // inserir msg de sucesso
+      
+    })
+    .catch((err) => {
+      res.redirect("404");
+    });
+});
 
-
-
-router.post("/articles/save",(req, res)=>{
-//  SAVAR ARTIGO
+router.post("/articles/save", (req, res) => {
+  //  SAlVAR ARTIGO
 
   let title = req.body.title;
   let body = req.body.body;
@@ -112,33 +106,68 @@ router.post("/articles/save",(req, res)=>{
     title: title,
     slug: slugify(title),
     body: body,
-    categoryId: category
-  }).then(()=>{
-    res.redirect("/admin/articles")
-  })
-
-})
-
-router.post("/articles/delete", (req,res)=>{
-  let id = req.body.id;
-  
-if(id != undefined){
-    if(!isNaN(id)){
-
-      Article.destroy({
-        where:{
-          id:id
-        }
-      }).then(()=>{
-        res.redirect("/admin/articles");
-      })
-    }else{
-      res.redirect("/admin/articles");
-    } 
-}else{
-  res.redirect("/admin/articles");
-}
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
 });
 
+router.post("/articles/delete", (req, res) => {
+  // DELETAR ARTIGO
+  let id = req.body.id;
+
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
+
+router.get("/articles/page/:num", (req, res, next) => {
+  // PAGINACAO DE PAGINA
+  let page = req.params.num;
+  let offset = 0;
+
+  if (isNaN(page) || page <= 1) {
+    offset = 0;
+  } else {
+    offset = (parseInt(page) - 1) * 4;
+  }
+
+  Article.findAndCountAll({
+    limit: 4,
+    offset: offset,
+    order: [["id", "DESC"]],
+  }).then((articles) => {
+    let next;
+    if (offset + 4 >= articles.count) {
+      next = false;
+    } else {
+      next = true;
+    }
+
+    let result = {
+      page:parseInt(page),
+      next: next,
+      articles: articles,
+    };
+
+    
+    Category.findAll().then((category) => {
+      res.render("admin/articles/page", { category: category, result: result });
+    });
+  });
+  
+});
 
 module.exports = router;
